788c5e5d341a620f4f0c94532b56e7a33cead7e6
William Neild <wneild5@gmail.com>
2024-02-18 02:02:13 +0000

    feat: implement emojis

--- apps/web/src/components/shared/markdown.tsx
+++ apps/web/src/components/shared/markdown.tsx
@@ -2,14 +2,28 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
-import rehypeSanitize from "rehype-sanitize";
+import rehypeSanitize, {defaultSchema, Options} from "rehype-sanitize";
+import remarkEmoji from "remark-emoji";
+import remarkTwemoji from "remark-twemoji";
 
 export default function Markdown({ content }: MarkdownProps) {
     return (
-        <div className="w-full max-w-full prose prose-slate prose-sm dark:prose-invert prose-img:rounded prose-headings:my-0.5 prose-p:my-0.5 prose-img:my-0.5">
+        <div className="w-full max-w-full prose prose-slate prose-sm dark:prose-invert prose-img:rounded prose-headings:my-0.5 prose-p:my-0.5 prose-img:my-0.5 prose-p:leading-6">
             <ReactMarkdown
-                remarkPlugins={[remarkGfm]}
-                rehypePlugins={[rehypeRaw, rehypeSanitize]}
+                remarkPlugins={[remarkGfm, remarkEmoji, [remarkTwemoji, {
+                    isReact: true,
+                    className: "inline-block w-6"
+                }]]}
+                rehypePlugins={[
+                    rehypeRaw,
+                    [rehypeSanitize, {
+                        ...defaultSchema,
+                        attributes: {
+                            ...defaultSchema.attributes,
+                            "img" : ["src", "longdesc", "loading", "alt", "className"],
+                        }
+                    } as Options]
+                ]}
             >
                 {content}
             </ReactMarkdown>
---
+++ apps/web/src/types/remark-twemoji.d.ts
@@ -0,0 +1,6 @@
+declare module 'remark-twemoji' {
+  import { Plugin } from 'unified';
+
+  const twemoji: Plugin;
+  export default twemoji;
+}
