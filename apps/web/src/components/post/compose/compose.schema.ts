99b364d7d494449f2477705ac85df4e128214268
William Neild <wneild5@gmail.com>
2024-02-18 00:39:35 +0000

    feat: bump to 512 chars

--- apps/web/src/components/post/compose/compose.schema.ts
+++ apps/web/src/components/post/compose/compose.schema.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const composePostSchema = z.object({
-    content: z.string().min(1).max(256),
+    content: z.string().min(1).max(512),
 });
 
 export type ComposePostForm = z.infer<typeof composePostSchema>;
