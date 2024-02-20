e634b7eb73742d97fa7d97b44c5b5eaaea3b3a47
William Neild <wneild5@gmail.com>
2024-02-20 16:32:18 +0000

    feat: link to profile on compose post avatar

--- apps/web/src/components/post/compose/compose-post.tsx
+++ apps/web/src/components/post/compose/compose-post.tsx
@@ -1,10 +1,13 @@
 import CurrentUserAvatar from "@/components/shared/current-user-avatar";
 import ComposeForm from "@/components/post/compose/compose-form";
+import Link from "next/link";
 
 export default function ComposePost() {
     return (
         <div className="flex gap-2">
-            <CurrentUserAvatar />
+            <Link href="/profile">
+                <CurrentUserAvatar />
+            </Link>
             <ComposeForm />
         </div>
     );
