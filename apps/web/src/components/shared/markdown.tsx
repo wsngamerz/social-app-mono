40b1b28cbe9e9ba5031562fc5fbc55408c3fad04
William Neild <wneild5@gmail.com>
2024-02-28 10:44:04 +0000

    fix: headings overflowing markdown container

--- apps/web/src/components/shared/markdown.tsx
+++ apps/web/src/components/shared/markdown.tsx
@@ -17,7 +17,8 @@ export default function Markdown({content, className}: MarkdownProps) {
     return (
         <div
             className={cn("prose prose-slate prose-sm dark:prose-invert",
-                "prose-headings:my-0.5 prose-img:my-0.5 prose-img:rounded",
+                "prose-headings:my-0.5 prose-headings:break-words",
+                "prose-img:my-0.5 prose-img:rounded",
                 "prose-p:my-0.5 prose-p:leading-6 prose-p:break-words",
                 "w-full max-w-full overflow-hidden",
                 className)}>
