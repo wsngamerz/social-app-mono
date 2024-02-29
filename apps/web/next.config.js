35eadd7205016f8a995aed7a41a36605c2f03c2c
William Neild <wneild5@gmail.com>
2024-02-29 20:03:04 +0000

    chore(dev): bundle analyser with env variable

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,14 +1,20 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  transpilePackages: ["@repo/ui"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "gravatar.com",
-        pathname: "/avatar/*"
-      }
-    ]
-  },
-  reactStrictMode: true
+const nextConfig = {
+    transpilePackages: ["@repo/ui"],
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "gravatar.com",
+                pathname: "/avatar/*"
+            }
+        ]
+    },
+    reactStrictMode: true
 };
+
+module.exports = withBundleAnalyzer(nextConfig)
